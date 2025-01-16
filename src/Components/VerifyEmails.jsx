eb830bec4c734d34f8b96eb0b8bdb7ecebb98721
temchelsy <148960005+temchelsy@users.noreply.github.com>
2025-01-16 11:10:31 +0100

    modified content

--- src/Components/VerifyEmails.jsx
+++ src/Components/VerifyEmails.jsx
@@ -6,9 +6,7 @@ const VerifyEmails = () => {
       <h1 className="text-center text-xl sm:text-2xl md:text-3xl font-semibold">
         Email Verification Link Sent Successfully
       </h1>
-      <h2 className="text-center text-sm sm:text-base md:text-lg mt-4">
-        Please check your inbox and click the link to verify your email. It expires in an hour.
-      </h2>
+      
     </div>
   );
 };
